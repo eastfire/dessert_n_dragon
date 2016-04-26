0cfdc1e6dd29dce34794ecb837744206bb08b938
eastfire <eastfire_he@hotmail.com>
2016-04-26 17:34:01 +0800

    Update texts.js

--- src/texts.js
+++ src/texts.js
@@ -88,7 +88,10 @@ var texts_locale = {
             "shop.horizontal-fire":"可以在商店中解锁横向火焰了",
             
             //shop
-            "card.horizontal-fire":"在无尽关卡中使用横向火焰"
+            "card.horizontal-fire":"在无尽关卡中使用横向火焰",
+            
+            //enemy
+            "enemy.chocolate-cake":"黑森林武士将在无尽关卡中出现"
         }
     },
     en: {
