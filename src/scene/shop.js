b663f09c3e318f91b0bbbc4e539216efe4d61491
eastfire <eastfire_he@hotmail.com>
2016-04-27 16:56:30 +0800

    Update shop.js

--- src/scene/shop.js
+++ src/scene/shop.js
@@ -19,6 +19,69 @@ var ShopLayer = cc.Layer.extend({
         this.scrollView.setInnerContainerSize(cc.size(this.scrollView.width, Math.max(this.scrollView.height, commodities.length * stepY+currentY)));
 
         this.addChild(this.scrollView);
+    },
+    
+    initMoney:function(){
+        this.moneyLabel = new ccui.Text("", "Arial", dimens.moneyLabel.fontSize );
+        this.moneyLabel.enableOutline(colors.moneyLabel.outline, dimens.moneyLabel.outlineWidth);
+        this.moneyLabel.setTextColor(colors.moneyLabel.inside);
+        this.moneyLabel.attr({
+            //color: colors.tableLabel,
+            x: dimens.moneyLabel.x,
+            y: dimens.moneyLabel.y,
+            anchorX: 0
+        });
+        this.addChild(this.moneyLabel);
+
+        var icon = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame( "icon-money.png" ))
+        icon.attr({
+            //color: colors.tableLabel,
+            x: dimens.moneyLabel.x - 15,
+            y: dimens.moneyLabel.y,
+            scaleX: 0.5,
+            scaleY: 0.5
+        });
+        this.addChild(icon);
+        this.renderMoney();
+    },
+    initMenu:function(){
+        var closeItem = new cc.MenuItemImage(
+            cc.spriteFrameCache.getSpriteFrame("close-default.png"),
+            cc.spriteFrameCache.getSpriteFrame("close-press.png"),
+            function () {
+                cc.director.runScene(new SelectRoomScene());
+            }, this);
+
+        closeItem.attr({
+            x: dimens.closeItem.x,
+            y: dimens.closeItem.y,
+            anchorX: 1,
+            anchorY: 1
+        });
+
+        var menu = new cc.Menu([closeItem]);
+        this.addChild(menu);
+        menu.attr({
+            x:0,
+            y:0,
+            anchorX:0,
+            anchorY: 0
+        })
+        if ( !unlockedStatus.isUnlocked("infinite") ) {
+            infiniteItem.opacity = 128;
+            infiniteItem.setEnabled(false);
+        }
+    },
+    renderMoney:function(){
+        this.moneyLabel.setString(gameStatus.get("money"));
+    },
+    onEnter:function(){
+        this._super();
+        gameStatus.on("change:money",this.renderMoney,this)
+    },
+    onExit:function(){
+        gameStatus.off("change:money")
+        this._super();
     }
 });
 
