73fd3030f7f27c94a9760c2659e26572379a332f
eastfire <eastfire_he@hotmail.com>
2016-05-09 10:25:33 +0800

    Update tile.js

--- src/model/tile.js
+++ src/model/tile.js
@@ -57,11 +57,31 @@ TILE_MODEL_MAP.portal = RoomTileModel.extend({
     defaults:function(){
         return _.extend(RoomTileModel.prototype.defaults.apply(this),{
             type: "portal",
-            subtype: "a"
+            subtype: "a",
+            canGenEnemy: false
         });
     },
     onTurnStart:function(){
-
+        //if has enemy
+        var movableModel = currentRoom.getMovableByTile(this);
+        if ( movableModel && 
+            movableModel.__teleportTurn !== currentRoom.get("turnNumber") && //防止反复传送
+            ( movableModel instanceof HeroModel || ( movableModel instanceof EnemyModel && movableModel.get("positions").length === 1 ) ) ) {
+            //find another same subtype
+            var tiles = currentRoom.filterTile(function(tileModel){
+                return tileModel !== this && tileModel.get("type") === this.get("type") &&
+                tileModel.get("subtype") === this.get("subtype");
+            },this);
+            if (tiles.length===1){
+                var targetMovable = currentRoom.getMovableByTile(tiles[0]);
+                if ( !targetMovable ) { //target portal is empty
+                    movableModel.__teleportTurn = currentRoom.get("turnNumber"); //防止反复传送
+                    movableModel.teleport(tiles[0].get("positions")[0]);
+                }
+            } else {
+                cc.error("stage error! portal not appear in pair.")
+            }
+        }
     }
 })
 
