8943a838b5df39b47590fbc570e1ace18f277093
eastfire <eastfire_he@hotmail.com>
2016-05-19 16:30:03 +0800

    Update infinite-room.js

--- src/model/infinite-room.js
+++ src/model/infinite-room.js
@@ -44,5 +44,31 @@ ROOM_MODEL_MAP.infinite = RoomModel.extend({
         return _.extend(RoomModel.prototype.defaults.call(this),{
             type : "infinite"
         })
-    }
+    },
+    switchRoom:function(){
+        var randomRoomEntry = _.sample(rooms)
+        this.set("initTiles", clone(randomRoomEntry.initTiles) );
+        this.initTiles();
+
+        _.each(this.__movables,function(movableModel){
+            if ( !(movableModel instanceof HeroModel) ) {
+                movableModel.destroy();
+            }
+        },this)
+        this.__movables = [];
+        this._hero.set("positions", clone(randomRoomEntry.initHero.positions));
+        this.__movables.push(this.__hero);
+        
+        this.__genMovableMap();
+        
+        this.trigger("switch-room",this);
+    },
+    initialize:function(){
+        if ( this.get("turnNumber") === 0 ) {
+            var randomRoomEntry = _.sample(rooms)
+            this.set("initTiles", clone(randomRoomEntry.initTiles) );
+            this.get("initHero").positions = clone(randomRoomEntry.initHero.positions);
+        }
+        RoomModel.prototype.initialize.call(this);
+    },
 });
