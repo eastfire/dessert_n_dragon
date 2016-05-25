17e9351fc036ef9f9864cc8a2fd96510a5414515
eastfire <eastfire_he@hotmail.com>
2016-05-25 10:16:11 +0800

    Update perk.js

--- src/model/perk.js
+++ src/model/perk.js
@@ -1,7 +1,8 @@
 var PERK_MAP = {};
 
-var hookManager = new HookManager();
+var hookManager = new Backbone.Model();
 
+//advantage
 PERK_MAP.addHalfHp = Backbone.Model.extend({
     initialize:function(){
         CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT*1.5;
@@ -11,3 +12,31 @@ PERK_MAP.addHalfHp = Backbone.Model.extend({
         Backbone.Model.prototype.destroy.call(this)
     }
 })
+PERK_MAP.addHalfHp.value = -3;
+
+//disadvantage
+PERK_MAP.remove10InitHp = Backbone.Model.extend({
+    initialize:function(){
+        hookManager.on("before-game-start",this.effect, this)
+    },
+    effect:function(params, extraParams){
+        var hero = currentRoom.room.getHero()
+        hero.set("hp",Math.max(1,hero.get("hp")-10))
+    },
+    destroy:function(){
+        hookManager.off("before-game-start",this.effect, this)
+        Backbone.Model.prototype.destroy.call(this)
+    }
+})
+PERK_MAP.remove10InitHp.value = 1;
+
+PERK_MAP.removeHalfHp = Backbone.Model.extend({
+    initialize:function(){
+        CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT*0.5;
+    },
+    destroy:function(){
+        CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT;
+        Backbone.Model.prototype.destroy.call(this)
+    }
+})
+PERK_MAP.removeHalfHp.value = 3;
