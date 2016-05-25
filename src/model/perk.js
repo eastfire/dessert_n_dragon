539c7100db02a996a4d99a04a3a4625e248ff17a
eastfire <eastfire_he@hotmail.com>
2016-05-25 14:00:12 +0800

    Update perk.js

--- src/model/perk.js
+++ src/model/perk.js
@@ -14,6 +14,21 @@ PERK_MAP.addHalfHp = Backbone.Model.extend({
 })
 PERK_MAP.addHalfHp.value = -3;
 
+PERK_MAP.moreChoice = Backbone.Model.extend({
+    initialize:function(){
+        hookManager.on("before-game-start",this.effect, this)
+    },
+    effect:function(params, extraParams){
+        var hero = currentRoom.room.getHero()
+        hero.set("choiceNumber", ORIGIN_CHOICE_NUMBER+1);
+    },
+    destroy:function(){
+        hookManager.off("before-game-start",this.effect, this)
+        Backbone.Model.prototype.destroy.call(this)
+    }
+})
+PERK_MAP.moreChoice.value = -2;
+
 //disadvantage
 PERK_MAP.remove10InitHp = Backbone.Model.extend({
     initialize:function(){
@@ -21,7 +36,7 @@ PERK_MAP.remove10InitHp = Backbone.Model.extend({
     },
     effect:function(params, extraParams){
         var hero = currentRoom.room.getHero()
-        hero.set("hp",Math.max(1,hero.get("hp")-10))
+        hero.set("hp",Math.max(1,Math.round(hero.get("hp")/2)))
     },
     destroy:function(){
         hookManager.off("before-game-start",this.effect, this)
@@ -40,3 +55,18 @@ PERK_MAP.removeHalfHp = Backbone.Model.extend({
     }
 })
 PERK_MAP.removeHalfHp.value = 3;
+
+PERK_MAP.lessChoice = Backbone.Model.extend({
+    initialize:function(){
+        hookManager.on("before-game-start",this.effect, this)
+    },
+    effect:function(params, extraParams){
+        var hero = currentRoom.room.getHero()
+        hero.set("choiceNumber", ORIGIN_CHOICE_NUMBER-1);
+    },
+    destroy:function(){
+        hookManager.off("before-game-start",this.effect, this)
+        Backbone.Model.prototype.destroy.call(this)
+    }
+})
+PERK_MAP.lessChoice.value = 2;
