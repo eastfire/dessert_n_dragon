10f5bc6e6dd1f2bc26c02fd7bfb38a629102bbbe
eastfire <eastfire_he@hotmail.com>
2016-05-25 17:50:01 +0800

    Update perk.js

--- src/model/perk.js
+++ src/model/perk.js
@@ -2,17 +2,19 @@ var PERK_MAP = {};
 
 var hookManager = new Backbone.Model();
 
+var PERK_LIST = ["halfHpMore", "moreChoice", "draw2",
+    "halfHpLess","halfInitHp","lessChoice"];
 //advantage
-PERK_MAP.addHalfHp = Backbone.Model.extend({
+PERK_MAP.halfHpMore = Backbone.Model.extend({
     initialize:function(){
-        CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT*1.5;
+        CONSTITUTION_EFFECT = CONSTITUTION_EFFECT+ORIGIN_CONSTITUTION_EFFECT*0.5; //in case halfHpMore and halfHpLess are both picked
     },
     destroy:function(){
         CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT;
         Backbone.Model.prototype.destroy.call(this)
     }
 })
-PERK_MAP.addHalfHp.value = -3;
+PERK_MAP.halfHpMore.value = 3;
 
 PERK_MAP.moreChoice = Backbone.Model.extend({
     initialize:function(){
@@ -27,10 +29,25 @@ PERK_MAP.moreChoice = Backbone.Model.extend({
         Backbone.Model.prototype.destroy.call(this)
     }
 })
-PERK_MAP.moreChoice.value = -2;
+PERK_MAP.moreChoice.value = 2;
+
+PERK_MAP.draw2 = Backbone.Model.extend({
+    initialize:function(){
+        hookManager.on("before-game-start",this.effect, this)
+    },
+    effect:function(params, extraParams){
+        var hero = currentRoom.room.getHero()
+        hero.set("drawEachTurn", 2);
+    },
+    destroy:function(){
+        hookManager.off("before-game-start",this.effect, this)
+        Backbone.Model.prototype.destroy.call(this)
+    }
+})
+PERK_MAP.draw2.value = 2;
 
 //disadvantage
-PERK_MAP.remove10InitHp = Backbone.Model.extend({
+PERK_MAP.halfInitHp = Backbone.Model.extend({
     initialize:function(){
         hookManager.on("before-game-start",this.effect, this)
     },
@@ -43,18 +60,18 @@ PERK_MAP.remove10InitHp = Backbone.Model.extend({
         Backbone.Model.prototype.destroy.call(this)
     }
 })
-PERK_MAP.remove10InitHp.value = 1;
+PERK_MAP.halfInitHp.value = -1;
 
-PERK_MAP.removeHalfHp = Backbone.Model.extend({
+PERK_MAP.halfHpLess = Backbone.Model.extend({
     initialize:function(){
-        CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT*0.5;
+        CONSTITUTION_EFFECT = CONSTITUTION_EFFECT-ORIGIN_CONSTITUTION_EFFECT*0.5; //in case halfHpMore and halfHpLess are both picked
     },
     destroy:function(){
         CONSTITUTION_EFFECT = ORIGIN_CONSTITUTION_EFFECT;
         Backbone.Model.prototype.destroy.call(this)
     }
 })
-PERK_MAP.removeHalfHp.value = 3;
+PERK_MAP.halfHpLess.value = -3;
 
 PERK_MAP.lessChoice = Backbone.Model.extend({
     initialize:function(){
@@ -69,4 +86,4 @@ PERK_MAP.lessChoice = Backbone.Model.extend({
         Backbone.Model.prototype.destroy.call(this)
     }
 })
-PERK_MAP.lessChoice.value = 2;
+PERK_MAP.lessChoice.value = -2;
